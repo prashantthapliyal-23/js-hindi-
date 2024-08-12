3a5b3215e5c7e262c077e1cd180897295fccb3ba
prashantthapliyal-23 <prashantthapliyal331@gmail.com>
2024-08-12 15:50:25 +0000

    datatype summary

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,52 @@
+//DATATYPES
+
+         // Primitive
+
+// 7types : String,  Number, Booolean, Null, undifined, symbol, bigInt
+
+// String
+const score = 100
+
+//Number
+const scoreValue = 100.3
+
+//Booolean
+const isLoggedIn = false
+
+//Null
+const outsideTemp = null 
+
+//undifined
+let userEmail;
+
+//symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//bigInt
+const bigNumber = 233430430n
+
+
+          //Non Primitive
+
+//Array, objects, Functions
+
+//Array
+
+const heros = ["shaktiman", "nagraj", "doga", "captain steel"]
+
+
+//Objects
+
+let myObj = {
+    name: "parth",
+    age: 22,
+}
+
+
+//Functions
+const myFunction = function(){
+    console.log("hello world");
+}
